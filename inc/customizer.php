e21a92d833dd076dc273c09dbf00ebd024072dcd
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-03 21:06:19 +0300

    initial commit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Shoestrap Theme Customizer.
+ *
+ * @package Shoestrap
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function shoestrap_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'shoestrap_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function shoestrap_customize_preview_js() {
+	wp_enqueue_script( 'shoestrap_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'shoestrap_customize_preview_js' );
