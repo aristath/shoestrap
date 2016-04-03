0df9047e9bf59a42f76bcb0c3159b3d7b6834d35
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-03 22:05:01 +0300

    fix #1

--- inc/data/class-shoestrap-data-post.php
+++ inc/data/class-shoestrap-data-post.php
@@ -8,6 +8,10 @@ class Shoestrap_Data_Post extends Shoestrap_Data {
 		parent::$data['post'] = (array) $post;
 		// add permalink
 		parent::$data['post']['permalink'] = get_permalink( parent::$data['post']['ID'] );
+		// Do not expose the post password.
+		if ( isset( parent::$data['post']['post_password'] ) ) {
+			unset( parent::$data['post']['post_password'] );
+		}
 
 	}
 
