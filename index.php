ebf9e935b305b2d38b10f4bc4cfbff6fa730bfcb
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-05 00:03:39 +0300

    Revert "convert index.php to underscore.js"
    This reverts commit 012d1e81065ba59270be942b87b782946b9c02b3.

--- index.php
+++ index.php
@@ -15,21 +15,42 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main"></main><!-- #main -->
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+
+			<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-
-/**
- * Call the underscore.js template
- */
-shoestrap_templates()->add_template( array(
-	'tmpl'    => 'shoestrap-posts',
-	'path'    => locate_template( 'views/index.php' ),
-	'element' => '#main',
-	'data'    => Shoestrap_Data::get_data(),
-) );
-
-
 get_sidebar();
 get_footer();
