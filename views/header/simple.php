3a26fdb2cf5b982fd82373f520a7909bcd0ba583
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-07 20:50:33 +0300

    Markup improvement

--- views/header/simple.php
+++ views/header/simple.php
@@ -15,18 +15,17 @@
 			</ul>
 		</div>
 		<div class="top-bar-right">
+			<ul data-responsive-toggle="primary-menu" data-hide-for="medium">
+				<button class="menu-icon" type="button" data-toggle>
+					<span class="screen-reader-text">' . esc_attr_e( 'Menu', 'shoestrap' ) . '</span>
+				</button>
+			</ul>
 			<?php
 			// Add menu.
 			wp_nav_menu( array(
 				'container'      => false,
-				'menu_class'     => 'vertical large-horizontal menu dropdown',
-				'items_wrap'     => '
-					<ul data-responsive-toggle="primary-menu" data-hide-for="large">
-						<button class="menu-icon" type="button" data-toggle>
-							<span class="screen-reader-text">' . esc_attr__( 'Menu', 'shoestrap' ) . '</span>
-						</button>
-					</ul>
-					<ul id="%1$s" class="%2$s" data-dropdown-menu data-responsive-menu="drilldown medium-dropdown" data-disable-hover="true" data-click-open="true">%3$s</ul>',
+				'menu_class'     => 'vertical medium-horizontal menu dropdown',
+				'items_wrap'     => '<ul id="%1$s" class="%2$s" data-dropdown-menu data-responsive-menu="drilldown medium-dropdown" data-disable-hover="true" data-click-open="true">%3$s</ul>',
 				'walker'         => new Shoestrap_Topbar_Menu_Walker(),
 				'theme_location' => 'primary',
 				'menu_id'        => 'primary-menu',
