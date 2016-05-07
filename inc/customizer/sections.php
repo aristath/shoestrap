d29e5ca2bd039697d31c5850a876dfcce53d4b62
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-07 16:39:01 +0300

    description for the Layout section.

--- inc/customizer/sections.php
+++ inc/customizer/sections.php
@@ -3,5 +3,5 @@
 Shoestrap_Kirki::add_section( 'layout', array(
 	'priority'    => 10,
 	'title'       => esc_attr__( 'Layout', 'shoestrap' ),
-	'description' => esc_attr__( 'Layout options', 'shoestrap' ),
+	'description' => esc_attr__( 'You can change the width of the header, footer & main content areas separately, as well as choose a layout mode for your sidebars.', 'shoestrap' ),
 ));
