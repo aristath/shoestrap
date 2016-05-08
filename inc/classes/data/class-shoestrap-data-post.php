09122b03447ca39c764bf84cfda273865a014b83
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-08 13:46:08 +0300

    fixes #8

--- inc/classes/data/class-shoestrap-data-post.php
+++ inc/classes/data/class-shoestrap-data-post.php
@@ -7,6 +7,10 @@ class Shoestrap_Data_Post extends Shoestrap_Data {
 		global $post;
 		parent::$data['post'] = (array) $post;
 
+		// Process shortcodes in the content and excerpt.
+		parent::$data['post']['post_content'] = do_shortcode( parent::$data['post']['post_content'] );
+		parent::$data['post']['post_excerpt'] = do_shortcode( parent::$data['post']['post_excerpt'] );
+
 		// add permalink
 		parent::$data['post']['permalink'] = get_permalink( parent::$data['post']['ID'] );
 
