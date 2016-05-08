4c71ebb8c82e0c934b898d9f0882a026c24a40ea
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-08 11:12:44 +0300

    remove default custom-header implementation.

--- inc/customizer.php
+++ inc/customizer.php
@@ -13,7 +13,6 @@
 function shoestrap_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'shoestrap_customize_register' );
 
