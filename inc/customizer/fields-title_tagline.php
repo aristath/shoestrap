e4a668636a4a28310b5d0b38922532886eeac515
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-08 14:25:22 +0300

    Add a logo-align setting.

--- inc/customizer/fields-title_tagline.php
+++ inc/customizer/fields-title_tagline.php
@@ -15,3 +15,24 @@ Shoestrap_Kirki::add_field( array(
 		),
 	),
 ));
+
+Shoestrap_Kirki::add_field( array(
+	'type'        => 'dashicons',
+	'settings'    => 'logo_alignment',
+	'label'       => __( 'Logo alignment', 'shoestrap' ),
+	'section'     => 'title_tagline',
+	'default'     => 'left',
+	'priority'    => 8,
+	'choices'     => array(
+		'left'    => 'editor-alignleft',
+		'center'  => 'editor-aligncenter',
+		'right'   => 'editor-alignright',
+	),
+	'transport'   => 'auto',
+	'output'      => array(
+		array(
+			'element'  => '#site-branding',
+			'property' => 'text-align',
+		),
+	),
+));
