8335700a4a14cd66e6047e2d6327372f76bb3557
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-08 11:14:59 +0300

    modify color fields order.

--- inc/customizer/fields-colors.php
+++ inc/customizer/fields-colors.php
@@ -1,6 +1,31 @@
 <?php
 
 Shoestrap_Kirki::add_field( array(
+	'type'        => 'color',
+	'settings'    => 'background_color',
+	'label'       => __( 'Background Color', 'shoestrap' ),
+	'section'     => 'colors',
+	'default'     => '#FFFFFF',
+	'priority'    => 10,
+	'transport'   => 'auto',
+	'choices'     => array(
+		'alpha'   => false,
+	),
+	'transport'   => 'refresh',
+	'output'      => array(
+		array(
+			'element'  => array(
+				'body.custom-background',
+				'table thead',
+				'table tbody',
+				'table tfoot',
+			),
+			'property' => 'background-color',
+		),
+	),
+));
+
+Shoestrap_Kirki::add_field( array(
 	'type'        => 'color-palette',
 	'settings'    => 'primary_color',
 	'label'       => __( 'Primary Color', 'shoestrap' ),
@@ -9,7 +34,7 @@ Shoestrap_Kirki::add_field( array(
 	'priority'    => 10,
 	'transport'   => 'auto',
 	'choices'     => array(
-		'colors' => ( class_exists( 'Kirki_Helper' ) ) ? Kirki_Helper::get_material_design_colors( 'all' ) : array(),
+		'colors' => ( class_exists( 'Kirki_Helper' ) ) ? Kirki_Helper::get_material_design_colors( '400' ) : array(),
 		'size'   => 32,
 	),
 	'transport'   => 'refresh',
@@ -42,31 +67,6 @@ Shoestrap_Kirki::add_field( array(
 			'element'       => '.is-dropdown-submenu .is-dropdown-submenu-parent.opens-right > a::after',
 			'property'      => 'border-color',
 			'value_pattern' => 'transparent transparent transparent $'
-		),
-	),
-));
-
-Shoestrap_Kirki::add_field( array(
-	'type'        => 'color',
-	'settings'    => 'background_color',
-	'label'       => __( 'Background Color', 'shoestrap' ),
-	'section'     => 'colors',
-	'default'     => '#FFFFFF',
-	'priority'    => 10,
-	'transport'   => 'auto',
-	'choices'     => array(
-		'alpha'   => false,
-	),
-	'transport'   => 'refresh',
-	'output'      => array(
-		array(
-			'element'  => array(
-				'body.custom-background',
-				'table thead',
-				'table tbody',
-				'table tfoot',
-			),
-			'property' => 'background-color',
 		),
 	),
 ));
