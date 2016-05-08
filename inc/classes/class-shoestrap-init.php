9c88bef68d0ab42128c676a6e7a064e73a0820fd
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-08 13:22:31 +0300

    move theme_supports to the Init class.

--- inc/classes/class-shoestrap-init.php
+++ inc/classes/class-shoestrap-init.php
@@ -10,5 +10,71 @@ class Shoestrap_Init {
 		// Instantiates the Kirki_Enqueue object.
 		new Shoestrap_Enqueue();
 
+		// Add theme supports.
+		add_action( 'after_setup_theme', array( $this, 'add_theme_supports' ) );
+
+	}
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function add_theme_supports() {
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See https://developer.wordpress.org/themes/functionality/post-formats/
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside',
+			'image',
+			'video',
+			'quote',
+			'link',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'shoestrap_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Enable logo support.
+		add_theme_support( 'custom-logo', array(
+			'flex-width' => true,
+		) );
 	}
 }
