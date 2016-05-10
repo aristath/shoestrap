e1262dfeed661f71f4067b0c3c85a37952860c73
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-10 22:08:05 +0300

    header enqueueing & inline docs improvements.

--- inc/classes/class-shoestrap-enqueue.php
+++ inc/classes/class-shoestrap-enqueue.php
@@ -18,19 +18,49 @@ class Shoestrap_Enqueue {
 	 */
 	public function enqueue() {
 
+		/**
+		 * Enqueue the theme's styles.css.
+		 * This is recommended because we can add inline styles there
+		 * and some plugins use it to do exactly that.
+		 */
 		wp_enqueue_style(
 			'shoestrap-style',
 			get_stylesheet_uri()
 		);
+
+		/**
+		 * Enqueue app.css stylesheet.
+		 * Includes foundation CSS.
+		 */
 		wp_enqueue_style(
 			'shoestrap-app',
-			trailingslashit( get_stylesheet_directory_uri() ) . 'assets/css/app.css'
+			trailingslashit( get_template_directory_uri() ) . 'assets/css/app.css'
 		);
+		/**
+		 * Load header styles if they exist.
+		 */
 		$header_mode = get_theme_mod( 'header_mode', 'top-navbar' );
-		wp_enqueue_style(
-			'shoestrap-header-' . $header_mode,
-			trailingslashit( get_stylesheet_directory_uri() ) . 'assets/css/header-' . $header_mode . '.css'
-		);
+		$url = false;
+		if ( is_child_theme() ) {
+			$path = wp_normalize_path( get_stylesheet_directory() . 'assets/css/header-' . $header_mode . '.css' );
+			if ( file_exists( $path ) ) {
+				$url = trailingslashit( get_stylesheet_directory_uri() ) . 'assets/css/header-' . $header_mode . '.css';
+			}
+		}
+		if ( ! $url ) {
+			$path = wp_normalize_path( get_template_directory() . 'assets/css/header-' . $header_mode . '.css' );
+			if ( file_exists( $path ) ) {
+				$url = trailingslashit( get_template_directory_uri() ) . 'assets/css/header-' . $header_mode . '.css';
+			}
+		}
+		if ( $url ) {
+			wp_enqueue_style( 'shoestrap-header-' . $header_mode, $url );
+		}
+
+		/**
+		 * Enqueue skip-link-focus-fix.
+		 * Necessary for accessibility purposes.
+		 */
 		wp_enqueue_script(
 			'shoestrap-skip-link-focus-fix',
 			trailingslashit( get_template_directory_uri() ) . 'assets/js/skip-link-focus-fix.js',
@@ -38,6 +68,10 @@ class Shoestrap_Enqueue {
 			false,
 			true
 		);
+
+		/**
+		 * Enqueue Foundation-6 JS.
+		 */
 		wp_enqueue_script(
 			'foundation6',
 			trailingslashit( get_template_directory_uri() ) . 'assets/js/foundation.js',
@@ -45,6 +79,10 @@ class Shoestrap_Enqueue {
 			false,
 			true
 		);
+
+		/**
+		 * Enqueue shoestrap JS.
+		 */
 		wp_enqueue_script(
 			'shoestrap-app',
 			trailingslashit( get_template_directory_uri() ) . 'assets/js/app.js',
@@ -53,10 +91,16 @@ class Shoestrap_Enqueue {
 			true
 		);
 
+		/**
+		 * The comment-reply script.
+		 */
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 
+		/**
+		 * Enqueue script handling the underscore.js templating system.
+		 */
 		wp_enqueue_script(
 			'shoestrap-underscore-templating',
 			trailingslashit( get_template_directory_uri() ) . 'assets/js/_templating.js',
